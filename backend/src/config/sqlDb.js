ecbbd099a4327b34eefd8a54db6c119c68e11b0a
shrutisahu16 <sahushruti510@gmail.com>
2026-03-29 17:18:33 +0530

    sqlDb change

--- backend/src/config/sqlDb.js
+++ backend/src/config/sqlDb.js
@@ -1,17 +1,33 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+// pool.connect()
+//   .then(() => console.log("PostgreSQL connected ✅"))
+//   .catch(err => console.error("PostgreSQL error ❌", err.message));
+
+// module.exports = pool;
+
+const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
   .then(() => console.log("PostgreSQL connected ✅"))
-  .catch(err => console.error("PostgreSQL error ❌", err.message));
+  .catch(err => console.error("PostgreSQL error ❌", err));
 
 module.exports = pool;
