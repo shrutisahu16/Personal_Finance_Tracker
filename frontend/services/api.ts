2fa9130a673ec4b631618c330bd8f42d910e78fb
shrutisahu16 <sahushruti510@gmail.com>
2026-03-29 19:23:32 +0530

    add api.ts

---
+++ frontend/services/api.ts
@@ -0,0 +1,8 @@
+// const API_URL = "http://localhost:5000/api";
+
+// export default API_URL;
+
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+
+export default API_URL;
